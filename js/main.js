40784fdcb842f4663902091757c4a5047bf147d7
eisbr11 <eisbr11@gmail.com>
2013-06-10 15:37:47 +0200

    added a stop button + new stand frame

--- js/main.js
+++ js/main.js
@@ -7,15 +7,22 @@ var animation_mover = {
         $('input#right').on('click', function(){
             animation_mover.move_to_right($('.guybrush'));
         });
+        $('input#stand').on('click', function(){
+            animation_mover.stand($('.guybrush'));
+        });
     },
     move_to_left : function(gb){
-        gb.stop().removeClass('right').addClass('left');
+        gb.stop().removeClass('stand').removeClass('right').addClass('walk').addClass('left');
         animation_mover.move_right_pix(gb, -80);
     },
 
     move_to_right : function(gb) {
-        gb.stop().removeClass('left').addClass('right');
+        gb.stop().removeClass('stand').removeClass('left').addClass('walk').addClass('right');
         animation_mover.move_right_pix(gb, 80);
+    },
+    
+    stand : function(gb) {
+        gb.stop().removeClass('walk').removeClass('left').removeClass('right').addClass('stand');
     },
     
     move_right_pix : function(gb, pix){
